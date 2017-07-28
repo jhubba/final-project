8fb5642417c161eec1c1f1cfe7657d15d720872b
Jorge Bracamonte <jorbraken@gmail.com>
2017-07-28 16:03:40 -0600

    Adding action to load symbols on main page

---
+++ src/controller/actions/StaticSymbolsAction.java
@@ -0,0 +1,51 @@
+package controller.actions;
+
+import com.opensymphony.xwork2.ActionSupport;
+import model.*;
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.struts2.ServletActionContext;
+
+public class StaticSymbolsAction extends ActionSupport {
+	
+	private static final long serialVersionUID = 2398066631321170705L;
+	private String[] symbols;
+	private float[] prices;
+	private float[] changes;
+	
+	@Override
+	public String execute() throws Exception {
+		
+		QueryHelper helper = new QueryHelper();
+		QueryResult query = (QueryResult) helper.getQuote(new String[] {"voo","iyy","vti"});
+		ArrayList<Result> quotes = (ArrayList<Result>) query.results;  
+		
+		if (quotes != null)
+		{
+			int i = 0;
+			for (Result r : quotes)
+			{
+				symbols[i] = r.symbol;
+				prices[i] = r.lastPrice;
+				changes[i] = r.netChange;
+				i++;
+			}
+			HttpServletRequest request = ServletActionContext.getRequest();
+			request.getRequestDispatcher("index.jsp").forward(ServletActionContext.getRequest(), ServletActionContext.getResponse());
+		}
+		return SUCCESS;
+	}
+	
+	public String footerQuotes() {
+		StringBuilder sb = new StringBuilder();
+		
+		for (int i = 0; i < symbols.length; i++) {
+			sb.append("<div class='col-sm-4'> <span class='label label-default'>")
+				.append(symbols[i].toUpperCase()).append("</span><span name='' class=''>")
+				.append(prices[i] + " " + changes[i]).append("</span></div>");
+		}
+		return sb.toString();
+	}
+}
