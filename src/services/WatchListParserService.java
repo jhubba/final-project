3d7aa61d22d9eb86222dbbde0cd48609117bf132
Bryan Fritz <bryanfritz1978@gmail.com>
2017-08-05 21:10:41 -0600

    added class that will be able to add and remove symbols from a text string. Will be used to add and remove from a user watchlist

---
+++ src/services/WatchListParserService.java
@@ -0,0 +1,40 @@
+package services;
+
+import java.util.List;
+
+import model.WatchListBean;
+import model.WatchListsBean;
+
+public class WatchListParserService {
+	private static final Object instance = new Object();
+	
+	protected WatchListParserService (){
+		
+	}
+	public static Object getInstance(){
+		return instance;
+	}
+	
+	public String watchListAdd(String sym, WatchListBean watchlist){
+		StringBuilder sb = new StringBuilder();
+		sb.append(watchlist.getWatchlist() + "," + sym);		
+		return sb.toString();
+	}
+	
+	public String watchListRemove(String sym, WatchListBean watchlist){
+		StringBuilder sb = new StringBuilder();
+		String[] syms = watchlist.getWatchlist().split(",");
+
+		for(String s : syms){
+			if(!s.equals(sym)){
+				sb.append(s +",");
+
+			}
+		}
+		sb.deleteCharAt(sb.length() - 1);
+		
+		
+		return sb.toString();
+	}
+	
+}
