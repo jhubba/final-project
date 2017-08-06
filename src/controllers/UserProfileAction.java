6e693724b83ed9e3544a27a6d27c5695659e94b8
Bryan Fritz <bryanfritz1978@gmail.com>
2017-08-06 02:58:26 -0600

    edited UserProfileAction to be able to support needed functionality for the userprofile.jsp page and building a watchlist

--- src/controllers/UserProfileAction.java
+++ src/controllers/UserProfileAction.java
@@ -1,13 +1,20 @@
 package controllers;
 
 import org.apache.struts2.ServletActionContext;
-
 import com.opensymphony.xwork2.ActionSupport;
-
+import model.WatchListBean;
+import model.WatchListQuoteHolderBean;
+import model.WatchListsBean;
+import services.SQLHelper;
+import services.WatchListSymbolHelper;
 
 public class UserProfileAction extends ActionSupport{
 	private static final long serialVersionUID = 2650084195542815421L;
 	private String username = ServletActionContext.getRequest().getSession().getAttribute("user").toString();
+	private String watchlistName;
+	private WatchListBean wlb;
+	private String symbols;
+	private WatchListQuoteHolderBean wlqhb;
 	
 	public String getUsername() {
 		return username;
@@ -15,10 +22,46 @@ public class UserProfileAction extends ActionSupport{
 	public void setUsername(String username) {
 		this.username = username;
 	}
+	public String getWatchlistName() {
+		return watchlistName;
+	}
+	public void setWatchlistName(String watchlistName) {
+		this.watchlistName = watchlistName;
+	}
+	public WatchListBean getWlb() {
+		return wlb;
+	}
+	public void setWlb(WatchListBean wlb) {
+		this.wlb = wlb;
+	}
+	public String getSymbols() {
+		return symbols;
+	}
+	public void setSymbols(String symbols) {
+		this.symbols = symbols;
+	}
+	public WatchListQuoteHolderBean getWlqhb() {
+		return wlqhb;
+	}
+	public void setWlqhb(WatchListQuoteHolderBean wlqhb) {
+		this.wlqhb = wlqhb;
+	}
 
-	@Override
-	public String execute(){
-		   
+	public String loadWatchList(){
+		WatchListsBean wlsb = new WatchListsBean();
+		wlsb.setUsername(getUsername());
+ 	   	wlsb.setWatchlists(SQLHelper.getUserWatchLists(getUsername())); 
+ 	   	setWlb(wlsb.getWatchListName(getWatchlistName()));   	
+ 	   	setSymbols(getWlb().getWatchlist());
+ 	   	setWlqhb(WatchListSymbolHelper.getSym(getSymbols()));
+ 	   	
+	   	ServletActionContext.getRequest().getSession().setAttribute("getTheQuotes", getWlqhb().getWatchListHolder());
+		ServletActionContext.getRequest().getSession().setAttribute("loadWatchList", wlsb.getWatchListName(getWatchlistName()));
 		return "SUCCESS";
 	}
+	
+	
+	
+	
+	
 }
