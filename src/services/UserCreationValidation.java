044356ad1fb612297c365d69b738c9720a5bcde3
Joe Hubbard <jwubba@gmail.com>
2017-08-07 10:45:00 -0600

    Checks if username exists in database before the user can create a new user this method should be called on the proposed username.

---
+++ src/services/UserCreationValidation.java
@@ -0,0 +1,36 @@
+package services;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import model.DatabaseConnectionFactory;
+
+public class UserCreationValidation {
+
+/**
+ * Takes in string username. tests if the username already exists in the database
+ * 
+ * @param username
+ * @return boolean, if username exists returns true and user needs to try another username.
+ */
+	
+public static Boolean doesUsernameExist(String username){
+		
+		java.sql.Connection conn = DatabaseConnectionFactory.getInstance().getDatabaseConnection() ;
+		PreparedStatement st = null;
+		boolean rs = false;
+		if(conn!=null){
+			try{
+				st = conn.prepareStatement("Select * from users where username = ?");	
+				st.setString(1, username);
+				rs = st.execute();	
+				conn.close();
+					
+			}
+			catch(SQLException e){
+				
+			}
+		}
+		return rs;
+	}
+}
