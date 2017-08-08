c400b9a9d54b9370dca8be8484765300849a6926
Bryan Fritz <bryanfritz1978@gmail.com>
2017-08-08 00:27:32 -0600

    Took methods that were in UserCreationValidation and moved them to the SQLHelper which is where our Database functions outside of user validation are to be stored. I also Fied the doesUsernName method as it was always returning true. Switched the createdUser to return a boolean instaed of int.

--- src/services/SQLHelper.java
+++ src/services/SQLHelper.java
@@ -39,9 +39,65 @@ public class SQLHelper {
 			conn.close();					
 		}
 		catch(SQLException e){
-			System.out.println("Error processing Nice Try \n" + e.toString());
+			System.out.println("Error processing getUserWatchLists Nice Try \n" + e.toString());
 		}
 		
 		return watchLists;
 	}
+	
+	public static boolean createUser(String username, String password, String firstname, String lastname, String email){
+		java.sql.Connection conn = DatabaseConnectionFactory.getInstance().getDatabaseConnection() ;
+		PreparedStatement st = null;
+		
+		try{
+			
+			st = conn.prepareStatement("insert into users (username, passwordHash, first_name, last_name, email) values (?, ?, ?, ?, ?)");	
+			st.setString(1, username);
+			st.setInt(2, password.hashCode());
+			st.setString(3, firstname);
+			st.setString(4, lastname);
+			st.setString(5, email);
+			st.executeUpdate();	
+			if(doesUsernameExist(username)){
+				conn.close();
+				return true;
+				
+			}
+//			while(rs.next()){				
+//				WatchListBean wb = new WatchListBean();
+//				wb.setWatchListName(rs.getString("wl_name"));
+//				wb.setWatchlist(rs.getString("stockSym"));
+//				watchLists.add(wb);
+//			}		
+			conn.close();					
+		}
+		catch(SQLException e){
+			System.out.println("Error processing Create User Nice Try \n" + e.toString());
+		}
+
+		return false;
+	}
+	public static boolean doesUsernameExist(String username){		
+			java.sql.Connection conn = DatabaseConnectionFactory.getInstance().getDatabaseConnection() ;
+			PreparedStatement st = null;
+			boolean returnStatus = false;
+			
+			if(conn!=null){
+				try{
+					st = conn.prepareStatement("Select * from users where username = ?");	
+					st.setString(1, username);
+					ResultSet rs = st.executeQuery();
+					if(rs.next()){
+						conn.close();
+						return true;
+					}
+					conn.close();
+						
+				}
+				catch(SQLException e){
+					System.out.println("Error processing doesUsernameExist Nice Try \n" + e.toString());
+				}
+			}
+			return returnStatus;
+		}
 }
