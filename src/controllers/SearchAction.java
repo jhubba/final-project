63b7c3bdfe7a3e5d933ebf904b984a5a0d6608c1
Jorge Bracamonte <jorbraken@gmail.com>
2017-08-08 21:33:58 -0600

    Changed action to return Result object

--- src/controllers/SearchAction.java
+++ src/controllers/SearchAction.java
@@ -7,22 +7,29 @@ import model.Result;
 
 public class SearchAction extends ActionSupport {
 	private static final long serialVersionUID = -4703251445471173407L;
+	private Result query;
 	private String symbol;
 
-	public String execute() {
-		ServletActionContext.getRequest().getSession().getAttribute(getSymbol());
+	public String execute() {		
 		QueryHelper helper = new QueryHelper();
-		Result query;
 		try {
-			query = (Result) helper.getSingleQuote("fb");
+			query = (Result) helper.getSingleQuote(getSymbol());
 			ServletActionContext.getRequest().getSession().setAttribute("query", query);
+			return "found";
 		} catch (Exception e) {
 			e.printStackTrace();
-			return null;
+			return "null";
 		}
-		return "found";
 	}
 	
+	public Result getQuery() {
+		return query;
+	}
+
+	public void setQuery(Result query) {
+		this.query = query;
+	}
+
 	public String getSymbol() {
 		return symbol;
 	}
